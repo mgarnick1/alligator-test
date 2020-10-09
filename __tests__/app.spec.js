13d21843503c0262f9f55a281a693c975697427b
Mark <mgarnick@techtonicgroup.com>
2020-10-09 10:53:54 -0600

    first test pass

---
+++ __tests__/app.spec.js
@@ -0,0 +1,9 @@
+/* eslint-disable no-undef */
+// import { mount } from "@vue/test-utils";
+import App from "../src/App.vue";
+
+describe("App", () => {
+  it("has data", () => {
+    expect(typeof App.data).toBe("function");
+  });
+});
