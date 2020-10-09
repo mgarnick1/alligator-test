573dd37db575f018689b977cf26dce7f6f001c79
Mark <mgarnick@techtonicgroup.com>
2020-10-09 10:41:32 -0600

    got initial setup

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <main>
+      <h3>Let us test your arithmetic.</h3>
+      <p>What is the sum of the two numbers.</p>
+      <div>
+        <p>{{ x1 }} + {{ x2 }} =</p>
+        <input v-model="guess" /><button @click="check">Check Answer</button>
+      </div>
+      <button @click="refresh">Refresh</button>
+      <p>{{ message }}</p>
+    </main>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      x1: Math.ceil(Math.random() * 100),
+      x2: Math.ceil(Math.random() * 100),
+      guess: "",
+      message: "",
+    };
+  },
+  methods: {
+    check() {
+      if (this.x1 + this.x2 === parseInt(this.guess)) {
+        this.message = "Success!";
+      } else {
+        this.message = "Try Again";
+      }
+    },
+    refresh() {
+      this.x1 = Math.ceil(Math.random() * 100);
+      this.x2 = Math.ceil(Math.random() * 100);
+    },
+  },
+};
 </script>
 
 <style>
